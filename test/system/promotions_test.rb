7ceb2608e4c8fdea86a3fc5fc475d98abc7a9e27
Lucas <lucas.yoiti95@hotmail.com>
2021-03-21 18:49:10 -0300

    Aula 3

---
+++ test/system/promotions_test.rb
@@ -0,0 +1,25 @@
+require 'application_system_test_case'
+
+class PromotionsTest < ApplicationSystemTestCase
+  test 'create promotion' do
+    visit root_path
+    click_on 'Promoções'
+    click_on 'Registrar uma promoção'
+    fill_in 'Nome', with: 'Cyber Monday'
+    fill_in 'Descrição', with: 'Promoção de Cyber Monday'
+    fill_in 'Código', with: 'CYBER15'
+    fill_in 'Desconto', with: '15'
+    fill_in 'Quantidade de cupons', with: '90'
+    fill_in 'Data de término', with: '22/12/2033'
+    click_on 'Criar promoção'
+
+    assert_current_path promotion_path(Promotion.last)
+    assert_text 'Cyber Monday'
+    assert_text 'Promoção de Cyber Monday'
+    assert_text '15,00%'
+    assert_text 'CYBER15'
+    assert_text '22/12/2033'
+    assert_text '90'
+    assert_link 'Voltar'
+  end
+end
