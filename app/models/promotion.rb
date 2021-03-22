8ac7371ddee12d0efd1aede161d1c9d03c464b4f
Lucas <lucas.yoiti95@hotmail.com>
2021-03-22 11:10:25 -0300

    Aula 4

--- app/models/promotion.rb
+++ app/models/promotion.rb
@@ -1,2 +1,12 @@
 class Promotion < ApplicationRecord
+  has_many :coupons 
+
+  validates :name, presence: { message: 'não pode ficar em branco'}
+  validates :code, presence: { message: 'não pode ficar em branco'}
+  validates :discount_rate, presence: { message: 'não pode ficar em branco'}
+  validates :coupon_quantity, presence: { message: 'não pode ficar em branco'}
+  validates :expiration_date, presence: { message: 'não pode ficar em branco'}
+  validates :code, uniqueness: { message: 'deve ser único'}
+  validates :name, uniqueness: { message: 'deve ser único'}
+
 end
--- test/system/promotions_test.rb
+++ test/system/promotions_test.rb
@@ -1,6 +1,83 @@
 require 'application_system_test_case'
 
 class PromotionsTest < ApplicationSystemTestCase
+  test 'view promotions' do
+    Promotion.create!(name: 'Natal', description: 'Promoção de Natal',
+                      code: 'NATAL10', discount_rate: 10, coupon_quantity: 100,
+                      expiration_date: '22/12/2033')
+    Promotion.create!(name: 'Cyber Monday', coupon_quantity: 100,
+                      description: 'Promoção de Cyber Monday',
+                      code: 'CYBER15', discount_rate: 15,
+                      expiration_date: '22/12/2033')
+
+    visit root_path
+    click_on 'Promoções'
+
+    assert_text 'Natal'
+    assert_text 'Promoção de Natal'
+    assert_text '10,00%'
+    assert_text 'Cyber Monday'
+    assert_text 'Promoção de Cyber Monday'
+    assert_text '15,00%'
+  end
+
+  test 'view promotion details' do
+    Promotion.create!(name: 'Natal', description: 'Promoção de Natal',
+                      code: 'NATAL10', discount_rate: 10, coupon_quantity: 100,
+                      expiration_date: '22/12/2033')
+    Promotion.create!(name: 'Cyber Monday', coupon_quantity: 90,
+                      description: 'Promoção de Cyber Monday',
+                      code: 'CYBER15', discount_rate: 15,
+                      expiration_date: '22/12/2033')
+
+    visit root_path
+    click_on 'Promoções'
+    click_on 'Cyber Monday'
+
+    assert_text 'Cyber Monday'
+    assert_text 'Promoção de Cyber Monday'
+    assert_text '15,00%'
+    assert_text 'CYBER15'
+    assert_text '22/12/2033'
+    assert_text '90'
+  end
+
+  test 'no promotion are available' do
+    visit root_path
+    click_on 'Promoções'
+
+    assert_text 'Nenhuma promoção cadastrada'
+  end
+
+  test 'view promotions and return to home page' do
+    Promotion.create!(name: 'Natal', description: 'Promoção de Natal',
+                      code: 'NATAL10', discount_rate: 10, coupon_quantity: 100,
+                      expiration_date: '22/12/2033')
+
+    visit root_path
+    click_on 'Promoções'
+    click_on 'Voltar'
+
+    assert_current_path root_path
+  end
+
+  test 'view details and return to promotions page' do
+    Promotion.create!(name: 'Natal', description: 'Promoção de Natal',
+                      code: 'NATAL10', discount_rate: 10, coupon_quantity: 100,
+                      expiration_date: '22/12/2033')
+
+    visit root_path
+    click_on 'Promoções'
+    click_on 'Natal'
+    click_on 'Voltar'
+
+    assert_current_path promotions_path
+  end
+end
+
+require 'application_system_test_case'
+
+class PromotionsTest < ApplicationSystemTestCase
   test 'create promotion' do
     visit root_path
     click_on 'Promoções'
@@ -21,5 +98,50 @@ class PromotionsTest < ApplicationSystemTestCase
     assert_text '22/12/2033'
     assert_text '90'
     assert_link 'Voltar'
+  end
+end
+
+require 'application_system_test_case'
+
+class PromotionsTest < ApplicationSystemTestCase
+  test 'create and attributes cannot be blank' do
+    visit root_path
+    click_on 'Promoções'
+    click_on 'Registrar uma promoção'
+    click_on 'Criar promoção'
+
+    assert_text 'não pode ficar em branco', count: 5
+  end
+
+  test 'create and code/name must be unique' do
+    Promotion.create!(name: 'Natal', description: 'Promoção de Natal',
+                      code: 'NATAL10', discount_rate: 10, coupon_quantity: 100,
+                      expiration_date: '22/12/2033')
+
+    visit root_path
+    click_on 'Promoções'
+    click_on 'Registrar uma promoção'
+    fill_in 'Código', with: 'NATAL10'
+    fill_in 'Nome', with: 'Natal'
+    click_on 'Criar promoção'
+
+    assert_text 'deve ser único', count: 2
+  end
+
+  test 'generate coupons for a promotion' do
+    promotion = Promotion.create!(name: 'Natal', description: 'Promoção de Natal',
+                      code: 'NATAL10', discount_rate: 10, coupon_quantity: 100,
+                      expiration_date: '22/12/2033')
+    visit promotion_path(promotion)
+    click_on 'Gerar cupons'
+
+    assert_text 'Cupons gerados com sucesso'
+    assert_no_link 'Gerar cupons'
+    assert_text 'NATAL10-0001'
+    assert_text 'NATAL10-0050'
+    assert_text 'NATAL10-0100'
+    assert_no_text 'NATAL10-0000'
+    assert_no_text 'NATAL10-0101'
+
   end
 end
