0aade0dec9897c13575a8114e67186b63c5a09e5
Lucas <lucas.yoiti95@hotmail.com>
2021-03-29 13:23:03 -0300

    Testes de erros ao cadastrar

--- test/system/authentication_test.rb
+++ test/system/authentication_test.rb
@@ -46,8 +46,65 @@ class AuthenticationTest < ApplicationSystemTestCase
     assert_text 'Você saiu da sua conta.'
   end
 
-  # TODO: erros no cadastrar
+  test 'empty input field - sign up' do
+    visit root_path
+    click_on 'Cadastrar'
+    fill_in 'Email', with: ''
+    fill_in 'Senha', with: ''
+    fill_in 'Confirmação de senha', with: ''
+    click_on 'Finalizar Cadastro'
+
+    assert_text 'Email não pode ficar em branco'
+    assert_text 'Senha não pode ficar em branco'
+    assert_current_path user_registration_path
+  end
+
+  test 'min password length' do
+    visit root_path
+    click_on 'Cadastrar'
+    fill_in 'Email', with: 'testando@iugu.com.br'
+    fill_in 'Senha', with: '1'
+    fill_in 'Confirmação de senha', with: '1'
+    click_on 'Finalizar Cadastro'
+
+    assert_text 'Senha é muito curto (mínimo: 6 caracteres)'
+  end
+
+  test 'max password length' do
+    visit root_path
+    click_on 'Cadastrar'
+    fill_in 'Email', with: 'testando@iugu.com.br'
+    fill_in 'Senha', with: '12345678901234567890'
+    fill_in 'Confirmação de senha', with: '12345678901234567890'
+    click_on 'Finalizar Cadastro'
+
+    assert_text 'Senha é muito longa (máximo: 15 caracteres)'
+  end
+
+  test 'email already in use' do
+    usuario = User.create!(email: 'usuario01@iugu.com.br', password: 'pass123')
+
+    visit root_path
+    click_on 'Cadastrar'
+    fill_in 'Email', with: usuario.email
+    fill_in 'Senha', with: usuario.password
+    fill_in 'Confirmação de senha', with: usuario.password
+    click_on 'Finalizar Cadastro'
+
+    assert_text 'Email já está em uso'
+  end
+
+  test 'confirm password' do
+    visit root_path
+    click_on 'Cadastrar'
+    fill_in 'Email', with: 'testando@iugu.com.br'
+    fill_in 'Senha', with: 'pass123'
+    fill_in 'Confirmação de senha', with: 'pass345'
+    click_on 'Finalizar Cadastro'
+
+    assert_text 'Confirmação de senha não é igual a Senha'
+  end
+
   # TODO: erro ao logar
   # TOOD: Usuario.name
-
 end
