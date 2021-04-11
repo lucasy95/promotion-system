f134b09b49b92aad5428a6bbcf4e1ede57b1c61c
Lucas <lucas.yoiti95@hotmail.com>
2021-04-11 20:00:20 -0300

    Adicionado gem SimpleCov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,11 +1,18 @@
 ENV['RAILS_ENV'] ||= 'test'
+require 'simplecov'
+
+SimpleCov.start 'rails' do
+  add_filter 'jobs'
+  add_filter 'mailers'
+end
+
 require_relative "../config/environment"
 require "rails/test_help"
 
 class ActiveSupport::TestCase
   include Warden::Test::Helpers
   # Run tests in parallel with specified workers
-  parallelize(workers: :number_of_processors)
+  #parallelize(workers: :number_of_processors)
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   # fixtures :all
